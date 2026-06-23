0c052c4d0fbb3b080e528df98e8b5d5512c85d30
DasariBullaiah <dasaridevops2025@gmail.com>
2026-06-23 23:10:31 +0530

    terraform-ansible integration

---
+++ terraform/main.tf
@@ -0,0 +1,47 @@
+resource "aws_security_group" "web" {
+
+    name = "web-sg"
+
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["10.0.0.0/8"] # Restrict to VPN/Bastion
+    }
+
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+
+resource "aws_instance" "web" {
+
+    count = var.instance_count
+
+    ami           = "ami-0521cb2d60cfbb1a6" # Amazon Linux 2023
+    instance_type = var.instance_type
+
+    key_name = var.key_name
+
+    vpc_security_group_ids = [
+        aws_security_group.web.id
+    ]
+
+    tags = {
+        Name        = "web-${count.index}"
+        Environment = "prod"
+        Role        = "web"
+        ManagedBy   = "terraform"
+    }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "instance_count" {
+    default = 3
+}
+
+variable "instance_type" {
+    default = "t3.micro"
+}
+
+variable "key_name" {
+    default = "devops"
+}
