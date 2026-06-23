1b694c8a9bf986835517101c8fcf77cc464ab0c8
DasariBullaiah <dasaridevops2025@gmail.com>
2026-06-23 23:14:49 +0530

    updated terraform-ansible integration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,28 +1,28 @@
-resource "aws_security_group" "web" {
+# resource "aws_security_group" "web" {
 
-    name = "web-sg"
+#     name = "web-sg"
 
-    ingress {
-        from_port   = 22
-        to_port     = 22
-        protocol    = "tcp"
-        cidr_blocks = ["10.0.0.0/8"] # Restrict to VPN/Bastion
-    }
+#     ingress {
+#         from_port   = 22
+#         to_port     = 22
+#         protocol    = "tcp"
+#         cidr_blocks = ["10.0.0.0/8"] # Restrict to VPN/Bastion
+#     }
 
-    ingress {
-        from_port   = 80
-        to_port     = 80
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+#     ingress {
+#         from_port   = 80
+#         to_port     = 80
+#         protocol    = "tcp"
+#         cidr_blocks = ["0.0.0.0/0"]
+#     }
 
-    egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-}
+#     egress {
+#         from_port   = 0
+#         to_port     = 0
+#         protocol    = "-1"
+#         cidr_blocks = ["0.0.0.0/0"]
+#     }
+# }
 
 
 resource "aws_instance" "web" {
@@ -34,9 +34,9 @@ resource "aws_instance" "web" {
 
     key_name = var.key_name
 
-    vpc_security_group_ids = [
-        aws_security_group.web.id
-    ]
+    # vpc_security_group_ids = [
+    #     aws_security_group.web.id
+    # ]
 
     tags = {
         Name        = "web-${count.index}"
